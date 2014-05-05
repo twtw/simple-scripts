4a416cc61b99ab65cbe96fd8af296c1bb51f7092
twtw <github@tagbible.net>
2014-05-05 17:49:02 +0800

    create chineset image by dialog

---
+++ dialog_cht_png.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+text=$(zenity --title="文字內容" --text "輸入文字：" --entry)
+output=$(zenity --title="輸出檔名" --text "輸出檔名：" --entry)
+font=$(zenity --height=500 --width=800 --title="選擇字型" --text "請選擇" --list --radiolist \
+  --column="選擇" --column="字檔" --column="名" --print-column=3 \
+  TRUE wqy-micro /usr/share/fonts/wenquanyi/wqy-microhei/wqy-microhei.ttc \
+  FALSE wqy-zen /usr/share/fonts/wenquanyi/wqy-zenhei/wqy-zenhei.ttc \
+  FALSE 明體 /usr/share/fonts/TTF/uming.ttc \
+  FALSE 楷體 /usr/share/fonts/TTF/ukai.ttc)
+backcolor=$(zenity --title="背景色" --color-selection)
+forecolor=$(zenity --title="前景色" --color-selection)
+echo $text $output $font $backcolor $forecolor
+
+convert -background "$backcolor" -fill "$forecolor" -font $font \
+       -size 128x128 -gravity center label:"$text" output/$output
--- rdesktop.sh
+++ rdesktop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ip=$(zenity --title="遠端桌面連線" --text "輸入遠端IP：" --entry)
-prog=$(zenity --height=300 --title="選擇連線方式" --text "請選擇" --list --radiolist --column="選擇" --column="線線" \
+prog=$(zenity --height=300 --title="選擇連線方式" --text "請選擇" --list --radiolist --column="選擇" --column="連線" \
   TRUE rdesktop \
 FALSE vncviewer )
 $prog $ip
